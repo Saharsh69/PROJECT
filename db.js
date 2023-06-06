e693e509dddd0bef1ff18c115cc9d65100d2028c
Saharsh69 <109021600+Saharsh69@users.noreply.github.com>
2023-06-06 14:40:13 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb://127.0.0.1:27017";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
